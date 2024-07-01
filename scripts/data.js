ae8cff139dbe12bd7cff75748d4f79f555b81633
John Doe <furqan6242@gmail.com>
2024-07-01 10:02:23 +0530

    Added Python Contents

--- scripts/data.js
+++ scripts/data.js
@@ -1,30 +1,27 @@
 const blogs = [
   {
-    title: "Wherever wow far thus a squirrel",
-    date: "March 3, 2016",
-    content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          debitis inventore libero adipisci quaerat reprehenderit. Quis enim
-          expedita, mollitia doloribus autem nemo officiis perferendis aperiam!
-          Quasi, quisquam! Voluptatibus, ut mollitia?`,
-
+    title: "Python",
+    date: "June 27, 2024",
+    content: `Being one of the most powerful, and versatile programming languages, 
+    Python is easy to learn and is widely used among developers. 
+    Its popularity and demand is increasing ever since its launch and still continuing to grow. 
+    Demand for Python developers has increased by 41% in the last 5 years.
+     Out of 25 million developers in the world, 8.2 million ar..`,
     btnLink: `pagelinks/python.html`,
   },
   {
-    title: "Wherever wow far thus a squirrel",
-    date: "March 3, 2016",
-    content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          debitis inventore libero adipisci quaerat reprehenderit. Quis enim
-          expedita, mollitia doloribus autem nemo officiis perferendis aperiam!
-          Quasi, quisquam! Voluptatibus, ut mollitia?`,
+    title: "JavaScript",
+    date: "June 26, 2024",
+    content: `JavaScript (JS) is a lightweight, dynamic programming language primarily used for web development. 
+    It’s a core technology of the web, alongside HTML and CSS. Here are some key points about JavaScript:`,
     btnLink: `pagelinks/about.html`,
   },
   {
-    title: "Wherever wow far thus a squirrel",
-    date: "March 3, 2016",
-    content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          debitis inventore libero adipisci quaerat reprehenderit. Quis enim
-          expedita, mollitia doloribus autem nemo officiis perferendis aperiam!
-          Quasi, quisquam! Voluptatibus, ut mollitia?`,
+    title: "PHP",
+    date: "June 23, 2024",
+    content: `The term PHP is an acronym for PHP: Hypertext Preprocessor. 
+    PHP is a server-side scripting language designed specifically for web development. 
+    It is open-source which means it is free to download and use. It is very simple to learn and use. The files...`,
     btnLink: `pagelinks/about.html`,
   },
   {
@@ -86,10 +83,10 @@ const blogs = [
 const socials = [
   {
     link: {
-      link_1: `#`,
+      link_1: `https://x.com/EGould3770`,
       link_2: `https://github.com/SuperSimpledev-alt`,
-      link_3: `#`,
-      link_4: `furqan.pioneer@stellar.com`,
+      link_3: `https://www.linkedin.com/in/furqan-rajput-667374289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app `,
+      link_4: `furqan.pioneer@gmail.com`,
     },
 
     social_icons: {
@@ -105,10 +102,14 @@ const socials = [
 
 const dataPy = [
   {
-    h1: `Wherever wow far thus a squirrel`,
-    p: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          debitis inventore libero adipisci quaerat reprehenderit. Quis enim
-          expedita, mollitia doloribus autem nemo officiis perferendis aperiam!
-          Quasi, quisquam! Voluptatibus, ut mollitia?`,
+    h1: `What is Python?`,
+    p: `Python is a programming language and the most simple one to start with. 
+    In doubt about the power it possesses; well it’s equal to or stronger than all other programming 
+    languages out there. It is a really great language to learn. Why? Well just because it is really
+    simple and has simple syntax and also an intuitive way of working with!`,
+    p2: `If you are coming from any other programming language you’ll probably pick it up just fine.
+         But there are various approaches that are radically different from the other programming languages.`,
+    h6: `Procedure To Download`,
+    a: ` python.org. `,
   },
 ];
--- scripts/python.js
+++ scripts/python.js
@@ -1,10 +1,72 @@
 let pyDoc = ``;
 dataPy.forEach((data) => {
   pyDoc += `
+
+  <div class="python-topics">
+        
+          <ul>
+            <a href="../pagelinks/python.html"><li>Python Intro</li><a>
+            <a href="#"><li>Python Get Started</li><a>
+            <a href="#"><li>Python Syntax</li><a>
+            <a href="#"><li>Python Comments</li><a>
+            <a href="#"><li>Python Varriables</li><a>
+            <a href="#"><li>Python DataTypes</li><a>
+            <a href="#"><li>Python Numbers</li><a>
+            <a href="#"><li>Python Casting</li><a>
+            <a href="#"><li>Python String</li><a>
+            <a href="#"><li>Python Boolean</li><a>
+            <a href="#"><li>Python Operators</li><a>
+            <a href="#"><li>Python Lists</li><a>
+            <a href="#"><li>Python Tuples</li><a>
+            <a href="#"><li>Python Sets</li><a>
+            <a href="#"><li>Python Dictionaries</li><a>
+            <a href="#"><li>if...Else</li><a>
+            <a href="#"><li>Python While Loop</li><a>
+            <a href="#"><li>Python For Loop</li><a>
+            <a href="#"><li>Funtions</li><a>
+            <a href="#"><li>Lamda</li><a>
+            <a href="#"><li>Arrays</li><a>
+            <a href="#"><li>Classes and Objects</li><a>
+            <a href="#"><li>Inheritance</li><a>
+            <a href="#"><li>Iterators</li><a>
+            <a href="#"><li>Polymorphism</li><a>
+            <a href="#"><li>Python Scope</li><a>
+            <a href="#"><li>Python Modules</li><a>
+            <a href="#"><li>Python Dates</li><a>
+            <a href="#"><li>Python Math</li><a>
+            <a href="#"><li>Python User-input</li><a>
+          </ul>
+        </a>
+      </div>
+  
     <div id="blog-post">
         <h1 class="blog-post-h1">${data.h1}</h1>
         <p class="blog-post-p">${data.p}</p>
-      </div>`;
+        <p class="blog-post-p">${data.p2}</p>
+
+        <div>
+        <h3 class="blog-post-p"> ${data.h6} </h3>
+        <span> Go to: </span>
+        <a href="https://www.python.org/downloads/" target= "blank"> ${data.a}  <a>
+        <span>Get it for whatever operating system you are using! </span>
+
+<p class="blog-post-p">Secondly, installing visual studio code: search VS code from your browser.
+If you already have the prefered one then it's just fine. <u> and make sure you have the python extension needed to run the code </u></p>
+ 
+<h3> Your first hello world programme in Python. </h3>
+
+<div class="python-code-example">
+      <p> <span class="programme-color">print<span> <span class = "programme-statement">("Hello World!")<span></p>
+   
+    <img width='50%'  src='../img/output.png' />
+      </div>
+
+        </div>
+
+     
+      
+      
+      `;
 });
 
-document.querySelector('.js-py-data').innerHTML = pyDoc;
+document.querySelector(".js-py-data").innerHTML = pyDoc;
